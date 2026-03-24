70df937157c9ae1253a073849b17a1f8e9bd409c
Volodymyr Kolosov <kolosovvova0@gmail.com>
2026-03-24 11:58:07 +0000

    case-transformer-fix

--- src/createServer.js
+++ src/createServer.js
@@ -8,36 +8,36 @@ function createServer() {
   const server = http.createServer((req, res) => {
     const [pathname, search] = req.url.split('?');
 
-    const toCase = new URLSearchParams(search).get('toCase') || '';
+    const toCase = new URLSearchParams(search || '').get('toCase') || '';
     const text = pathname.slice(1);
-    const falidate = {
+    const validate = {
       errors: [],
     };
 
     if (text.trim() === '') {
-      falidate.errors.push({
+      validate.errors.push({
         message:
           'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
       });
     }
 
     if (toCase === '') {
-      falidate.errors.push({
+      validate.errors.push({
         message:
           '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
       });
     } else if (!CASE_NAME.includes(toCase)) {
-      falidate.errors.push({
+      validate.errors.push({
         message:
           'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
       });
     }
 
-    if (falidate.errors.length !== 0) {
+    if (validate.errors.length !== 0) {
       res.statusCode = 400;
       res.setHeader('Content-type', 'application/json');
       res.statusMessage = 'Bad request';
-      res.end(JSON.stringify(falidate));
+      res.end(JSON.stringify(validate));
 
       return;
     }
